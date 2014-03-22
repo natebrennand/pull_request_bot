764f54c27536c08c482c9a4fc3c3426df5098235
Nate Brennand <natebrennand@gmail.com>
2014-03-22 18:32:59 -0400

    finish github integration

--- bot/github/listen.go
+++ bot/github/listen.go
@@ -1,42 +1,90 @@
 package github
 
 import (
+	"../configure"
+
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
+
+var KEYWORDS = [2]string{"lgtm", "merge it"}
 
 type Action struct {
 	Repository struct {
-		Name      string
-		Full_name string
-		Owner     User
+		Name     string `json:"name"`
+		FullName string `json:"full_name"`
+		Owner    User
 	}
-	Sender       User
-	Pull_request PullRequest
-	Comment      Comment
-	Action       string
+	Issue struct {
+		Number int    `json:"number"`
+		Title  string `json:"title"`
+	}
+	Sender      User
+	PullRequest PullRequest `json:"pull_request"`
+	Comment     Comment     `json:"comment"`
+	Action      string      `json:"action"`
 }
 
 type PullRequest struct {
-	Title     string
-	Body      string
-	Html_url  string
-	Merged    bool
-	Mergeable bool
-	Comments  int
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	HtmlUrl   string `json:"html_url"`
+	Merged    bool   `json:"merged"`
+	Mergeable bool   `json:"mergeable"`
+	Comments  int    `json:"comments"`
 }
 
 type Comment struct {
-	Body     string
-	Html_url string
-	User     User
+	Body    string `json:"body"`
+	HtmlUrl string `json:"html_url"`
+	User    User
 }
 
 type User struct {
-	Login string
-	Id    int
+	Login string `json:"login"`
+	Id    int    `json:"id"`
+}
+
+func (c Comment) RequestApproved (approvers []string) bool {
+	for _, approver := range approvers {
+		if approver == c.User.Login {
+			for _, keyword := range KEYWORDS {
+				if strings.Contains(c.Body, keyword) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func CheckIssue (owner, repo string, comments []Comment, config []configure.Repo) bool {
+	approvalsNeeded :=  0
+	approvers := []string{}
+
+	for _, relevantRepo := range config {
+		if relevantRepo.Name == repo {
+			approvalsNeeded = relevantRepo.ApprovalsNeeded
+			approvers = relevantRepo.Approvers
+		}
+	}
+	if approvalsNeeded == 0 {	// repo not in config
+		return false
+	}
+
+	for _, comment := range comments {
+		if comment.RequestApproved(approvers) {
+			approvalsNeeded -= 1
+		}
+	}
+
+	if approvalsNeeded > 0 {
+		return false
+	}
+	return true
 }
 
 // read in request data to a Action struct
@@ -44,7 +92,6 @@ func ParseData(req *http.Request) (Action, error) {
 	// parse bytes from req object
 	bodyBytes, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		fmt.Println(err.Error())
 		return Action{}, err
 	}
 
@@ -59,12 +106,34 @@ func ParseData(req *http.Request) (Action, error) {
 	return body, nil
 }
 
+func CheckIssueComments(token, owner, repo string, issueNumber int, config []configure.Repo) error {
+	uri := "/repos/"+repo+"/issues/"+strconv.Itoa(issueNumber)+"/comments"
+	respBody, err := GithubAPICall(token, uri, "GET", nil)
+	if err != nil {
+		return err
+	}
+	var comments []Comment
+	json.Unmarshal(respBody, &comments)
+
+	if CheckIssue(owner, repo, comments, config) {
+		return MergePullRequest(token, owner, repo, issueNumber)
+	}
+	return nil
+}
+
 func HandleHook(req *http.Request) (int, string) {
 	body, err := ParseData(req)
 	if err != nil {
 		return 500, "SERVER ERROR: " + err.Error()
 	}
-	fmt.Printf("json: %#v\n", body)
+
+	config := configure.GlobalConfig
+	if body.Action == "created" {
+		err = CheckIssueComments(configure.GlobalEnv["github_token"], body.Repository.Owner.Login, body.Repository.FullName, body.Issue.Number, config)
+		if err != nil {
+			return 400, err.Error()
+		}
+	}
 
 	return 200, "RECEIVED!"
 }
--- bot/github/update.go
+++ bot/github/update.go
@@ -3,7 +3,6 @@ package github
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -19,30 +18,32 @@ type MergeRequestStruct struct {
 	CommitMessage string `json:"commit_message"`
 }
 
-func MergePullRequest(token, repo, owner string, requestNumber int) error {
-	uri := ("https://api.github.com/repos/" + owner + "/" + repo + "/pulls/" + strconv.Itoa(requestNumber) + "/merge")
-	fmt.Println(uri)
-
-	bodyBytes, err := json.Marshal(MergeRequestStruct{"merge bot!"})
+func GithubAPICall(token, uri, method string, data []byte) ([]byte, error) {
+	uri = "https://api.github.com" + uri
+	body := bytes.NewBuffer(data)
+	req, err := http.NewRequest(method, uri, body)
 	if err != nil {
-		return err
-	}
-	body := bytes.NewBuffer(bodyBytes)
-	req, err := http.NewRequest("PUT", uri, body)
-	if err != nil {
-		return err
+		return nil, err
 	}
 	req.Header.Add("Authorization", "token "+token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		return nil, err
+	}
+	return respBody, nil
+}
+
+func MergePullRequest(token, owner, repo string, requestNumber int) error {
+	uri := ("/repos/" + repo + "/pulls/" + strconv.Itoa(requestNumber) + "/merge")
+	bodyBytes, err := json.Marshal(MergeRequestStruct{"merge bot!"})
+	if err != nil {
 		return err
 	}
-
-	fmt.Println(string(respBody))
-	return nil
+	_, err = GithubAPICall(token, uri, "PUT", bodyBytes)
+	return err
 }
